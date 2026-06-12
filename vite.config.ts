1e26e91433331aff42567bd34b439ff337358336
kevinalvarel <muhammadkevinalvarel@gmail.com>
2026-06-12 16:54:30 +0700

    [fix] : Fix deployment plugin with Netlify Package

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,16 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   server: {
-    allowedHosts: [
-      "api.microlink.io",
-    ],
+    allowedHosts: ['api.microlink.io'],
   },
   plugins: [
     devtools(),
+    netlify(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tailwindcss(),
     tanstackStart(),
